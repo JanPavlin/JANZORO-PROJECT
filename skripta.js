d26ebd8892775d381d03339e0d29615d68f625f2
urosZoretic <uros.zoretic123@gmail.com>
2016-09-08 22:15:50 +0200

    Update skripta.js

--- skripta.js
+++ skripta.js
@@ -1,34 +1,12 @@
 var main = function() {
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0%"
-    }, 200);
-
-    $('body').animate({
-      left: "10%"
-    }, 200);
-    
-     
-         $("img").css("background-color", "rgba(255, 255, 255, 0.4)");
-     
-    
-  });
-
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-10%"
-    }, 200);
-
-    $('body').animate({
-      left: "0%"
-    }, 200);
-    
-  $("img").css("background-color", "white");
+  $('.glavni').click(function() {
+    $('.menu').toggle();
         
   });
   
-    
 };
 
 
 $(document).ready(main);
+
+
